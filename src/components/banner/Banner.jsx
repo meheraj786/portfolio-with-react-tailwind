d7198bff5fca2af42d6e6754ff4ef046f4a15ca8
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-22 01:19:22 +0600

    banner add

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Container from '../../layouts/Container'
+import RoundedElemText from '../../layouts/RoundedElemText'
+import { FiDownload } from "react-icons/fi";
+import Flex from '../../layouts/Flex'
+import BannerShape from '../../assets/BannerShape';
+
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className="banner pt-[103px] pb-[88px] bg-dark-bg">
+      <Container>
+        <Flex>
+          <div className="banner_left text-left md:w-[50%] font-secondary">
+            <h5 className='text-[32px] font-medium text-primary'>Hello I'm</h5>
+            <h1 className='font-secondary mt-[27px] mb-[12px] text-[98px] font-bold text-white'>Meheraj Hosen</h1>
+            <h5 className='text-[32px] font-primary font-medium text-white'>I'm <span className='text-primary'>MERN Stack Developer</span></h5>
+            <p className='text-body mt-[27px] mb-[54px] text-white md:w-[450px] '>A passionate web developer based from Bangladesh.With a keen eye for detail and a passion for web design, I specialize in creating visually captivating and highlyfunctional websites</p>
+          <RoundedElemText svg={<FiDownload />
+}>Download My Resume</RoundedElemText>
+          </div>
+          <div className="banner_right flex justify-center md:w-[50%]">
+            <BannerShape/>
+          </div>
+        </Flex>
+
+      </Container>
+
+    </div>
   )
 }
 
--- src/layouts/RoundedElemText.jsx
+++ src/layouts/RoundedElemText.jsx
@@ -3,7 +3,7 @@ import RoundIconBox from './RoundIconBox'
 
 const RoundedElemText = ({children, className, svg}) => {
   return (
-    <button className={`flex cursor-pointer justify-center group hover:text-primary items-center gap-2 ${className}`}>{children}<RoundIconBox className="group-hover:bg-bg group-hover:text-primary">{svg}</RoundIconBox></button>
+    <button className={`flex cursor-pointer justify-center group text-white hover:text-primary items-center gap-2 ${className}`}>{children}<RoundIconBox className="group-hover:bg-bg group-hover:text-primary">{svg}</RoundIconBox></button>
   )
 }
 
