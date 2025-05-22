4da4f9006c3619fbdce198f4630d11f80e5d6b93
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-23 02:40:44 +0600

    service add

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from '../../layouts/Container'
+
+const Services = () => {
+  return (
+    <div className="services">
+      <Container>
+        <div className="title">
+          <p>Popular Services</p>
+          <h2>My Special Service For yourBusiness Development</h2>
+        </div>
+        
+      </Container>
+
+    </div>
+  )
+}
+
+export default Services
