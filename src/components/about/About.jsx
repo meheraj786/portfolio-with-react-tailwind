6def05940689222d9bcbefee30937160a9134ddc
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-24 16:01:20 +0600

    about responsive add

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <div className="about py-[150px] bg-dark-bg">
       <Container>
-        <Flex className="bg-black px-[77px] py-[91px] rounded-[10px]">
+        <Flex className="bg-black text-center md:text-left flex-col md:flex-row px-3 md:px-[77px] py-[91px] rounded-[10px]">
           <div className="about-left md:w-[50%]">
             <p className='text-body mb-2 text-white'>About Me</p>
             <h3 className='text-white text-subheading font-bold md:w-[510px] font-primary'>Professional <span className='text-primary'>Problem Solutions </span>
@@ -17,30 +17,26 @@ const About = () => {
 voluptatem accusantium doloremque
 laudantium, totam rem aperiamc</p>
 
-<Flex className="gap-[39px] mb-6 w-[511px]">
+<Flex className="md:gap-[39px] flex-col md:flex-row gap-y-5 md:gap-y-0 mb-6 md:w-[511px]">
   <RoundedElemText className="flex-row-reverse" svg={<GoNorthStar/>}>Front-end Design</RoundedElemText>
   <RoundedElemText className="flex-row-reverse" svg={<GoNorthStar/>}>Front-end Design</RoundedElemText>
 </Flex>
-<Flex className="gap-[39px] w-[511px]">
+<Flex className="md:gap-[39px] flex-col md:flex-row gap-y-5 md:gap-y-0 md:w-[511px]">
   <RoundedElemText className="flex-row-reverse" svg={<GoNorthStar/>}>Front-end Design</RoundedElemText>
   <RoundedElemText className="flex-row-reverse" svg={<GoNorthStar/>}>Front-end Design</RoundedElemText>
 </Flex>
 
           </div>
-          <div className="about-right relative md:w-[50%]">
-            <div className="img group border border-black hover:border hover:border-primary ms-auto relative bg-white max-w-[346px] h-[363px] font-semibold  rounded-[10px]">
+          <div className="about-right mt-5 md:mt-0 relative md:w-[50%]">
+            <div className="img group border border-black hover:border hover:border-primary ms-auto relative bg-white max-w-[346px] min-w-[300px] h-[363px] font-semibold  rounded-[10px]">
               <span className='bg-white text-black 
-              px-[15px] group-hover:border group-hover:border-primary py-[13px] rounded-[30px] absolute left-[-69px] bottom-[142px]'>MERN Stack Developer</span>
+              px-[15px] group-hover:border group-hover:border-primary py-[13px] rounded-[30px] absolute md:left-[-69px] left-1/2 -translate-1/2 md:-translate-0 md:bottom-[142px] bottom-[-40px]'>MERN Stack Developer</span>
               <span className='bg-white text-black 
-              px-[44px] group-hover:border group-hover:border-primary py-[13px] font-semibold  rounded-[30px] absolute left-[-152px] bottom-[46px]'>Meheraj Hosen</span>
-
+              px-[44px] group-hover:border group-hover:border-primary py-[13px] font-semibold  rounded-[30px] absolute md:left-[-152px] left-1/2 -translate-1/2 md:-translate-0 bottom-[-120px] md:bottom-[46px]'>Meheraj Hosen</span>
             </div>
-            <GoNorthStar className='text-primary w-[96px] h-[95px] absolute top-0 left-0'/>
-
-
+            <GoNorthStar className='text-primary w-[96px] h-[95px] hidden md:block absolute top-0 left-0'/>
           </div>
         </Flex>
-
       </Container>
 
     </div>
