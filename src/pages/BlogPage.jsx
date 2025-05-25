d4d3a1331696f34d51052832c2071f14a3bdcaa8
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-26 02:59:33 +0600

    responsive issue fix

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Container from "../layouts/Container";
+import Flex from "../layouts/Flex";
+import RoundedElemText from "../layouts/RoundedElemText";
+import { FiArrowUpRight } from "react-icons/fi";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import blog1 from "../assets/blog1.png";
 
-const Blog = () => {
+const BlogPage = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+  const blogs = [
+    {
+      date: "12 Feb 2023",
+      title: "How to build a responsive website",
+      link: "#",
+      image: blog1,
+    },
+    {
+      date: "12 Feb 2023",
+      title: "How to build a responsive website",
+      link: "#",
+      image: blog1,
+    },
+    {
+      date: "12 Feb 2023",
+      title: "How to build a responsive website",
+      link: "#",
+      image: blog1,
+    },
+    {
+      date: "12 Feb 2023",
+      title: "How to build a responsive website",
+      link: "#",
+      image: blog1,
+    },
+    {
+      date: "12 Feb 2023",
+      title: "How to build a responsive website",
+      link: "#",
+      image: blog1,
+    },
+    {
+      date: "12 Feb 2023",
+      title: "How to build a responsive website",
+      link: "#",
+      image: blog1,
+    },
+    {
+      date: "12 Feb 2023",
+      title: "How to build a responsive website",
+      link: "#",
+      image: blog1,
+    },
+    {
+      date: "12 Feb 2023",
+      title: "How to build a responsive website",
+      link: "#",
+      image: blog1,
+    },
+  ];
   return (
-    <div>Blog</div>
-  )
-}
+    <div id="blog" className="blog  relative overflow-x-hidden py-[150px] md:py-[207px] text-white bg-dark-bg">
+      <div className="circle-blue absolute w-[152px] h-[152px] right-[10%] top-[10%] animate-pulse rounded-full bg-[rgba(82,73,206,0.21)] shadow-[0_0_100px_100px_rgba(82,73,206,0.21)]"></div>
+      <Container>
+        <div data-aos="zoom-in" className="title text-center">
+          <p>News & Blog</p>
+          <h2 className="font-primary mt-[30px] mb-[76px] font-bold text-heading-sm md:text-heading">
+            Latest News & <span className="text-primary">Blog</span>
+          </h2>
+        </div>
+        <div data-aos="flip-up" className="blogs">
+          <Flex className="flex-wrap gap-y-[30px]">
+            {blogs.map((blog, index) => (
+              <Flex
+                key={index}
+                className="blog-item mx-auto md:gap-x-[38px] md:flex-row flex-col border max-w-[630px] border-black group hover:border-primary rounded-[20px] py-[18px] px-[20px] bg-black"
+              >
+                <div className="image rounded-[10px] w-[209px] h-[191px]  mb-5 md:mb-0 bg-amber-50" style={{
+    background: `url(${blog.image})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    overflow: 'hidden',
+  }}>
+                  <img className="object-cover" src={blog.image} alt="" />
+                </div>
+                <div className="content md:text-left text-center md:text-0 max-w-[395px]">
+                  <p>{blog.date}</p>
+                  <h3 className="group-hover:text-primary font-primary text-subheading-sm md:text-subheading font-bold mt-[3px] max-w-[295px] mb-[6px]">
+                    {blog.title}
+                  </h3>
+                  <a href={blog.link}>
+                    <RoundedElemText
+                      className="md:justify-start justify-center"
+                      svg={<FiArrowUpRight />}
+                    >
+                      Read More
+                    </RoundedElemText>
+                  </a>
+                </div>
+              </Flex>
+            ))}
+          </Flex>
+        </div>
 
-export default Blog
+      </Container>
+    </div>
+  );
+};
+
+export default BlogPage
