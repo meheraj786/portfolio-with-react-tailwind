54ec945dab5e6381d1dbc9cb489d73a773d62c3e
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-29 17:34:29 +0600

    content add

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Container from '../layouts/Container'
 
 const BlogDetails = () => {
   return (
@@ -6,13 +7,13 @@ const BlogDetails = () => {
       <Container>
         <div className="title mb-[50px] text-center">
           <p className='text-body'>31 February 2025</p>
-          <h1 className='font-primary mt-[30px] font-bold '>Beginner Guide of tech industry</h1>
+          <h1 className='font-primary mt-[30px] md:text-subheading text-subheading-sm font-bold '>Beginner Guide of tech industry</h1>
           </div>
-          <div className='content bg-black border border-primary rounded-[20px] py-[38px] px-[73px]'>
-    <div className="img max-w-[758px] h-[440px] bg-gray-300 rounded-[20px]"></div>
-    <p>
+          <div className='content bg-black border border-primary rounded-[20px]  py-[38px] px-[73px]'>
+    <div className="img max-w-[758px] mx-auto mb-[50px] h-[440px] bg-gray-300 rounded-[20px]"></div>
+    <p className='text-body-sm md:text-body'>
     Lorem ipsum odor amet, consectetuer adipiscing elit. Vehicula duis et feugiat vestibulum sollicitudin mattis nullam. Habitant risus lacus eros sem et efficitur phasellus. Nisi cubilia neque mi inceptos nam ut. Pulvinar hac velit consequat ipsum; rhoncus imperdiet per facilisis malesuada. Nibh magnis sed maecenas tempor eu morbi facilisis habitasse tristique. Vehicula torquent curae sodales porta inceptos molestie. Nascetur curabitur aenean facilisi est rutrum euismod.
-
+    
 Molestie lobortis litora morbi dolor aptent neque lorem. Nascetur luctus efficitur maecenas non diam nec elementum. Orci curabitur purus nam sagittis dis. In morbi purus aptent facilisis in volutpat at himenaeos amet. Sapien dictum dignissim praesent at vivamus. Penatibus commodo nostra luctus vulputate imperdiet orci. Risus magna fermentum in orci nibh bibendum. Euismod nostra cursus per dictum feugiat.
 
 Blandit non etiam lacus litora turpis cras. Elit eu nibh elit placerat fermentum libero justo consequat. Laoreet imperdiet mauris neque natoque risus turpis maximus odio orci. Libero pulvinar elementum integer aliquam lacus placerat mattis amet. Mollis dis condimentum duis hac condimentum vivamus nullam odio dapibus. Dolor mattis dictum dui neque suspendisse nisl orci. Fringilla lacinia vehicula a justo accumsan venenatis sagittis.
