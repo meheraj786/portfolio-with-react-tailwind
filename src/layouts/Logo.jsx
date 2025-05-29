12e72f3627fa0944dab89e2161634dca6b33c0c3
Mehraj Hosen <meherajhosen786@gmail.com>
2025-05-29 18:56:02 +0600

    link add

--- src/layouts/Logo.jsx
+++ src/layouts/Logo.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
+<Link to="/">
 <h1 className="md:text-[48px] text-[34px] font-extrabold text-transparent stroke-text w-fit">
       Meheraj
     </h1>
+</Link>
   );
 }
--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -11,20 +11,28 @@ const BlogDetails = ({blogs}) => {
 
   if (!blog) {
     return(
-    <div className="blogDetails bg-dark-bg py-[100px] pt-[150px] text-white flex justify-center items-center h-screen text-center">
+    <div className="blogDetails bg-dark-bg py-[100px] pt-[150px] text-white relative flex justify-center items-center h-screen text-center">
+            <div className="circle-violet absolute w-[152px] h-[152px] left-[10%] top-[40%] animate-pulse rounded-full bg-[rgba(47,17,30,0.48)] shadow-[0_0_100px_100px_rgba(47,17,30,0.48)]">
+        
+
+        </div>
       <h1 className='font-primary mt-[30px] md:text-subheading text-subheading-sm font-bold '>Blog not Found :(</h1>
       </div>
     )
   }
   return (
-          <div className="blogDetails bg-dark-bg py-[100px] pt-[200px] text-white">
+          <div className="blogDetails relative bg-dark-bg py-[100px] pt-[200px] text-white">
+                  <div className="circle-violet absolute w-[152px] h-[152px] left-[10%] top-[10%] animate-pulse rounded-full bg-[rgba(47,17,30,0.48)] shadow-[0_0_100px_100px_rgba(47,17,30,0.48)]">
+        
+
+        </div>
           <Container>
             <div className="title mb-[50px] text-center">
               <p className='text-body'>{blog.date}</p>
               <h1 className='font-primary mt-[30px] md:text-subheading text-subheading-sm font-bold '>{blog.title}</h1>
               </div>
-              <div className='content bg-black border border-primary rounded-[20px]  py-[38px] px-[73px]' >
-        <div className="img max-w-[758px] mx-auto mb-[50px] h-[440px] bg-gray-300 rounded-[20px]" style={{
+              <div className='content bg-black border border-primary rounded-[20px]  py-[38px] md:px-[73px] px-8' >
+        <div className="img md:max-w-[758px] w-full mx-auto mb-[50px] h-[440px] bg-gray-300 rounded-[20px]" style={{
       background: `url(${blog.image})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
