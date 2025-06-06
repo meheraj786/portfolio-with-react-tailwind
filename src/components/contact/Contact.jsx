231fc8979c7d659b224ac09c0e40f28ec1cd4331
Mehraj Hosen <meherajhosen786@gmail.com>
2025-06-06 21:59:35 +0600

    toast remove

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,37 +12,8 @@ import 'aos/dist/aos.css';
 const Contact = () => {
   const [show, setShow]= useState(false)
 
-  const successToast= 
-  (<div id="toast-success" className="flex absolute z-[99999] top-40 bg-bg left-1/2 -translate-x-1/2 items-center w-full max-w-xs p-4 mb-4  text-primary rounded-lg shadow-sm dark:text-gray-400 dark:bg-gray-800" role="alert">
-    <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-        </svg>
-        <span className="sr-only">Check icon</span>
-    </div>
-    <div className="ms-3 text-sm font-normal">Succesfull.</div>
-    <button type="button" onClick={()=>setShow(false)} className="ms-auto -mx-1.5 -my-1.5 bg-dark-bg text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
-        <span className="sr-only">Close</span>
-        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-        </svg>
-    </button>
-</div>)
-const unsuccessToast= (<div id="toast-danger" className="flex absolute z-[99999] top-40 bg-bg left-1/2 -translate-x-1/2 items-center w-full max-w-xs p-4 mb-4  text-primary rounded-lg shadow-sm dark:text-gray-400 dark:bg-gray-800" role="alert">
-  <div className="inline-flex items-center justify-center shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
-      </svg>
-      <span className="sr-only">Error icon</span>
-  </div>
-  <div className="ms-3 text-sm font-normal">Item has been deleted.</div>
-  <button onClick={()=>setShow(false)} type="button" className="ms-auto -mx-1.5 -my-1.5 bg-dark-bg text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
-      <span className="sr-only">Close</span>
-      <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-      </svg>
-  </button>
-</div>)
+
+
 
 useEffect(() => {
   const timer = setTimeout(() => {
@@ -58,7 +29,7 @@ useEffect(() => {
   const items= ["2 years of experience", "10+ Projects Completed", "5+ Clients Worked"]
   return (
     <>
-    {show && successToast}
+
     <div id='contact' className="contact relative overflow-x-hidden pt-[70px] md:py-[121px] font-secondary bg-dark-bg text-white">
       <div className="circle-violet absolute w-[152px] h-[152px] left-[10%] top-[40%] animate-pulse rounded-full bg-[rgba(47,17,30,0.48)] shadow-[0_0_100px_100px_rgba(47,17,30,0.48)]">
         
