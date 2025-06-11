e087572d4812dc99d7661ce382cd20f5e078bd79
Mehraj Hosen <meherajhosen786@gmail.com>
2025-06-11 20:48:39 +0600

    footer responsive fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -49,9 +49,9 @@ const Footer = () => {
   ];
   return (
     <>
-      <div className="footer bg-[url(assets/lines.png)] bg-cover overflow-x-hidden font-secondary text-center md:text-left text-white pt-[79px] bg-dark-bg">
+      <div className="footer bg-[url(assets/lines.png)] bg-cover overflow-x-hidden font-secondary text-center xl:text-left text-white pt-[79px] bg-dark-bg">
         <Container>
-          <Flex className="md:px-[88px] gap-y-5 md:gap-y-0 flex-col md:flex-row rounded-[20px] bg-black py-[104px]">
+          <Flex className="xl:px-[88px] gap-y-5 xl:gap-y-0 flex-col xl:flex-row rounded-[20px] bg-black py-[104px]">
             <div data-aos="fade-up" className="left">
               <Logo />
             </div>
@@ -81,7 +81,7 @@ const Footer = () => {
               <input
                 type="email"
                 placeholder="Enter Your Email"
-                className="border-b-1 mt-[50px] md:mr-4"
+                className="border-b-1 mt-[50px] xl:mr-4"
               />
               <button
                 className="bg-primary rounded-[20px] text-black text-[16px] font-medium cursor-pointer hover:bg-dark-bg hover:border-primary hover:text-primary border border-primary items-center gap-3 py-[10px] px-[20px]"
@@ -91,7 +91,7 @@ const Footer = () => {
               </button>
             </div>
             <div data-aos="fade-up" className="right">
-              <h4 className="text-[24px] mb-3 md:mb-[38px] text-medium">
+              <h4 className="text-[24px] mb-3 xl:mb-[38px] text-medium">
                 Address
               </h4>
               <ul>
@@ -105,7 +105,7 @@ const Footer = () => {
         </Container>
         <div className="footer_bottom font-secondary bg-dark-bg py-[40px]">
           <Container>
-            <Flex className="justify-between gap-y-4 md:gap-y-0 flex-col md:flex-row">
+            <Flex className="justify-between gap-y-4 xl:gap-y-0 flex-col xl:flex-row">
               <div className="left opacity-80">
                 <p>© {year} Meheraj. All rights reserved.</p>
               </div>
@@ -127,7 +127,7 @@ const Footer = () => {
           </Container>
         </div>
       </div>
-      <div className="whatsapp p-[10px] md:p-[20px] md:text-[50px] text-[30px] bg-primary text-bg cursor-pointer hover:text-primary fixed md:left-[100px] left-[50px] md:bottom-[100px] bottom-[35px] hover:bg-transparent rounded-full hvoer:border-primary border border-primary opacity-80 hover:opacity-100">
+      <div className="whatsapp p-[10px] xl:p-[20px] xl:text-[50px] text-[30px] bg-primary text-bg cursor-pointer hover:text-primary fixed xl:left-[100px] left-[50px] xl:bottom-[100px] bottom-[35px] hover:bg-transparent rounded-full hvoer:border-primary border border-primary opacity-80 hover:opacity-100">
         <a
           href="https://api.whatsapp.com/send?phone=1989162543&text=I%20want%20to%20know%20about%20your%20products?"
           target="blank"
